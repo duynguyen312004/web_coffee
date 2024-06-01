b5933d4af9c8409044aaeb06089ffded0eb8d160
Phu Nguyen <nguyenphu43kun@gmaill.com>
2024-06-01 18:06:19 +0700

    Cartr, Checkout-page, Product-page / 18:00

--- fe-web-coffee/js/GetProducts-logined.js
+++ fe-web-coffee/js/GetProducts-logined.js
@@ -1,18 +1,21 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
     // Gọi API để lấy danh sách sản phẩm
-    axios.get('http://localhost:8080/api/products')  // Thay URL bằng URL API thực tế của bạn
-        .then(response => {
+    axios
+        .get("http://localhost:8080/api/products") // Thay URL bằng URL API thực tế của bạn
+        .then((response) => {
             const products = response.data;
             // Render danh sách sản phẩm lên trang
             renderProducts(products);
         })
-        .catch(error => console.error('Error fetching products:', error));
+        .catch((error) => console.error("Error fetching products:", error));
 });
 
 function renderProducts(products) {
-    const containers = document.querySelectorAll('.row.row-cols-4.row-cols-md-1');
-    containers.forEach(container => {
-        container.innerHTML = ''; // Clear existing content
+    const containers = document.querySelectorAll(
+        ".row.row-cols-4.row-cols-md-1"
+    );
+    containers.forEach((container) => {
+        container.innerHTML = ""; // Clear existing content
     });
 
     products.forEach((product, index) => {
@@ -24,7 +27,7 @@ function renderProducts(products) {
                 <article class="cate-item">
                     <img src="data:image/jpeg;base64,${product.img_path}" alt="${product.name}" class="cate-item__thumb" />
                     <section class="cate-item__info">
-                        <a href="#!" class="cate-item__title">${product.name}</a>
+                        <a href="../view/product-detail.html" class="cate-item__title">${product.name}</a>
                         <p class="cate-item__desc">${product.price} đ</p>
                     </section>
                 </article>
